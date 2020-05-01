95301b72d9d4ff8916d462de0913e37ec10d70e5
Humaira Masdi <humairamasdi97@gmail.com>
2020-05-01 16:56:23 +0800

    Created 'Create Template'

--- ramadan-planner/src/app/tab3/tab3.page.ts
+++ ramadan-planner/src/app/tab3/tab3.page.ts
@@ -8,5 +8,32 @@ import { Component } from '@angular/core';
 export class Tab3Page {
 
   constructor() {}
+  
+  segmentChanged(ev: any) {
+    console.log('Segment changed', ev);
+  }
 
+  public form = [
+    { val: 'Pepperoni', isChecked: true },
+    { val: 'Sausage', isChecked: false },
+    { val: 'Mushroom', isChecked: false }
+  ];
+
+MySelect1:any=[];
+moreIndex1:any=1;
+doc_name:any=[];
+
+selectNo1(val1){
+    if(val1==1)
+    {
+     this.MySelect1.push(this.moreIndex1);
+     this.moreIndex1++;
+     this.doc_name++;
+    }
+    else{
+      this.MySelect1.pop(this.moreIndex1);
+      this.moreIndex1--;
+      this.doc_name--;
+    }    
+  }
 }
